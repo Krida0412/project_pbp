6f14370ed24d04b625e86f74af38d17a1e14f38c
Krida Mukti Eundang Setiaji <kridamuktieundangsetiaji.works@gmail.com>
2024-12-20 06:13:45 +0700

    update baru euy

--- main.py
+++ main.py
@@ -12,28 +12,26 @@ class ContactApp(App):
         # Layout utama untuk menampung semua elemen
         self.layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
-        # Bagian input (Form) di atas
+        # Bagian input (Form)
         input_layout = BoxLayout(orientation='vertical', size_hint_y=None, height=200, spacing=10)
         
         # TextInput untuk nama
         self.name_input = TextInput(
-            hint_text='Nama',
-            background_normal='',
+            hint_text='Nama',git 
             background_color=(218/255, 247/255, 166/255, 1),
             font_size=20,
             height=65,
-            size_hint_y=None  # Memastikan tinggi diatur berdasarkan height
+            size_hint_y=None
         )
 
         # TextInput untuk nomor telepon (hanya angka)
         self.phone_input = TextInput(
-            hint_text='Nomor Telepon',
-            background_normal='',
+            hint_text='Nomor Telepon',git 
             background_color=(0.9, 0.9, 0.9, 1),
             font_size=20,
             height=65,
-            size_hint_y=None,  # Memastikan tinggi diatur berdasarkan height
-            input_filter='int'  # Hanya menerima angka
+            size_hint_y=None,
+            input_filter='int'
         )
 
         # Tombol Add Contact
@@ -42,7 +40,7 @@ class ContactApp(App):
             background_color=(0.1, 0.6, 0.1, 1),
             font_size=18,
             height=50,
-            size_hint_y=None,  # Memastikan tinggi diatur berdasarkan height
+            size_hint_y=None,
             on_press=self.add_contact
         )
 
@@ -55,7 +53,7 @@ class ContactApp(App):
         self.layout.add_widget(input_layout)
 
         # ScrollView untuk daftar kontak
-        self.contacts_scroll = ScrollView(size_hint=(1, 1))  # Memastikan ScrollView memenuhi ruang yang tersisa
+        self.contacts_scroll = ScrollView(size_hint=(1, 1))
         self.contacts_grid = GridLayout(cols=1, size_hint_y=None)
         self.contacts_grid.bind(minimum_height=self.contacts_grid.setter('height'))
         self.contacts_scroll.add_widget(self.contacts_grid)
@@ -100,7 +98,7 @@ class ContactApp(App):
                 size_hint_x=0.2,
                 font_size=14,
                 background_color=(0.1, 0.5, 1, 1),
-                on_press=lambda c_id=contact[0], c_name=contact[1], c_phone=contact[2]: self.edit_contact(c_id, c_name, c_phone)
+                on_press=lambda instance, c_id=contact[0], c_name=contact[1], c_phone=contact[2]: self.edit_contact(c_id, c_name, c_phone)
             )
             contact_widget.add_widget(edit_button)
 

b4539bb4c5e6aa3322b1e1788547d805accec8e6
Krida Mukti Eundang Setiaji <kridamuktieundangsetiaji.works@gmail.com>
2024-12-20 06:56:26 +0700

    update paling terbaru gess

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 
-class ContactApp(App):
+class AplikasiKontak(App):
     def build(self):
         # Layout utama untuk menampung semua elemen
-        self.layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        self.layout_utama = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
         # Bagian input (Form)
-        input_layout = BoxLayout(orientation='vertical', size_hint_y=None, height=200, spacing=10)
+        layout_input = BoxLayout(orientation='vertical', size_hint_y=None, height=200, spacing=10)
         
         # TextInput untuk nama
-        self.name_input = TextInput(
+        self.input_nama = TextInput(
             hint_text='Nama',
             background_color=(218/255, 247/255, 166/255, 1),
             font_size=20,
@@ -24,8 +24,8 @@ class ContactApp(App):
             size_hint_y=None
         )
 
-        # TextInput untuk nomor telepon (hanya angka)
-        self.phone_input = TextInput(
+        # TextInput untuk nomor telepon
+        self.input_nomer = TextInput(
             hint_text='Nomor Telepon',
             background_color=(0.9, 0.9, 0.9, 1),
             font_size=20,
@@ -34,113 +34,113 @@ class ContactApp(App):
             input_filter='int'
         )
 
-        # Tombol Add Contact
-        self.add_button = Button(
+        # Tombol Tambah Kontak
+        self.tombol_tambah = Button(
             text='Tambah Kontak',
             background_color=(0.1, 0.6, 0.1, 1),
             font_size=18,
             height=50,
             size_hint_y=None,
-            on_press=self.add_contact
+            on_press=self.tambah_kontak
         )
 
-        # Menambahkan elemen ke input_layout
-        input_layout.add_widget(self.name_input)
-        input_layout.add_widget(self.phone_input)
-        input_layout.add_widget(self.add_button)
+        # Menambahkan elemen ke layout_input
+        layout_input.add_widget(self.input_nama)
+        layout_input.add_widget(self.input_nomer)
+        layout_input.add_widget(self.tombol_tambah)
 
-        # Menambahkan input_layout ke layout utama di bagian atas
-        self.layout.add_widget(input_layout)
+        # Menambahkan layout_input ke layout utama di bagian atas
+        self.layout_utama.add_widget(layout_input)
 
         # ScrollView untuk daftar kontak
-        self.contacts_scroll = ScrollView(size_hint=(1, 1))
-        self.contacts_grid = GridLayout(cols=1, size_hint_y=None)
-        self.contacts_grid.bind(minimum_height=self.contacts_grid.setter('height'))
-        self.contacts_scroll.add_widget(self.contacts_grid)
+        self.scroll_kontak = ScrollView(size_hint=(1, 1))
+        self.grid_kontak = GridLayout(cols=1, size_hint_y=None)
+        self.grid_kontak.bind(minimum_height=self.grid_kontak.setter('height'))
+        self.scroll_kontak.add_widget(self.grid_kontak)
 
         # Menambahkan ScrollView ke layout utama
-        self.layout.add_widget(self.contacts_scroll)
+        self.layout_utama.add_widget(self.scroll_kontak)
 
         # Memuat kontak dari database
-        self.load_contacts()
+        self.memuat_kontak()
 
-        return self.layout
+        return self.layout_utama
 
-    def add_contact(self, instance):
-        name = self.name_input.text
-        phone = self.phone_input.text
-        if name and phone:
+    def tambah_kontak(self, instance):
+        nama = self.input_nama.text
+        nomer = self.input_nomer.text
+        if nama and nomer:
             conn = sqlite3.connect('contacts.db')
             c = conn.cursor()
-            c.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
+            c.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (nama, nomer))
             conn.commit()
             conn.close()
-            self.name_input.text = ''
-            self.phone_input.text = ''
-            self.load_contacts()
+            self.input_nama.text = ''
+            self.input_nomer.text = ''
+            self.memuat_kontak()
 
-    def load_contacts(self):
-        self.contacts_grid.clear_widgets()
+    def memuat_kontak(self):
+        self.grid_kontak.clear_widgets()
         conn = sqlite3.connect('contacts.db')
         c = conn.cursor()
         c.execute('SELECT * FROM contacts')
-        contacts = c.fetchall()
+        daftar_kontak = c.fetchall()
         conn.close()
 
-        for contact in contacts:
-            contact_widget = BoxLayout(size_hint_y=None, height=50)
-            contact_label = Label(text=f'{contact[1]} - {contact[2]}', size_hint_x=0.6, font_size=18)
-            contact_widget.add_widget(contact_label)
+        for kontak in daftar_kontak:
+            widget_kontak = BoxLayout(size_hint_y=None, height=50)
+            label_kontak = Label(text=f'{kontak[1]} - {kontak[2]}', size_hint_x=0.6, font_size=18)
+            widget_kontak.add_widget(label_kontak)
 
             # Tombol Edit
-            edit_button = Button(
+            tombol_edit = Button(
                 text='Edit',
                 size_hint_x=0.2,
                 font_size=14,
                 background_color=(0.1, 0.5, 1, 1),
-                on_press=lambda instance, c_id=contact[0], c_name=contact[1], c_phone=contact[2]: self.edit_contact(c_id, c_name, c_phone)
+                on_press=lambda instance, c_id=kontak[0], c_nama=kontak[1], c_nomer=kontak[2]: self.edit_kontak(c_id, c_nama, c_nomer)
             )
-            contact_widget.add_widget(edit_button)
+            widget_kontak.add_widget(tombol_edit)
 
-            # Tombol Delete
-            delete_button = Button(
+            # Tombol Hapus
+            tombol_hapus = Button(
                 text='Hapus',
                 size_hint_x=0.2,
                 font_size=14,
                 background_color=(1, 0, 0, 1),
-                on_press=lambda instance, c_id=contact[0]: self.delete_contact(c_id)
+                on_press=lambda instance, c_id=kontak[0]: self.hapus_kontak(c_id)
             )
-            contact_widget.add_widget(delete_button)
+            widget_kontak.add_widget(tombol_hapus)
 
-            self.contacts_grid.add_widget(contact_widget)
+            self.grid_kontak.add_widget(widget_kontak)
 
-    def edit_contact(self, c_id, c_name, c_phone):
-        self.name_input.text = c_name
-        self.phone_input.text = c_phone
-        self.add_button.text = 'Perbarui kontak'
-        self.add_button.bind(on_press=lambda instance, c_id=c_id: self.update_contact(c_id))
+    def edit_kontak(self, c_id, c_nama, c_nomer):
+        self.input_nama.text = c_nama
+        self.input_nomer.text = c_nomer
+        self.tombol_tambah.text = 'Perbarui Kontak'
+        self.tombol_tambah.bind(on_press=lambda instance, c_id=c_id: self.perbarui_kontak(c_id))
 
-    def update_contact(self, c_id):
-        name = self.name_input.text
-        phone = self.phone_input.text
-        if name and phone:
+    def perbarui_kontak(self, c_id):
+        nama = self.input_nama.text
+        nomer = self.input_nomer.text
+        if nama and nomer:
             conn = sqlite3.connect('contacts.db')
             c = conn.cursor()
-            c.execute('UPDATE contacts SET name = ?, phone = ? WHERE id = ?', (name, phone, c_id))
+            c.execute('UPDATE contacts SET name = ?, phone = ? WHERE id = ?', (nama, nomer, c_id))
             conn.commit()
             conn.close()
-            self.name_input.text = ''
-            self.phone_input.text = ''
-            self.add_button.text = 'Tambah Kontak'
-            self.load_contacts()
+            self.input_nama.text = ''
+            self.input_nomer.text = ''
+            self.tombol_tambah.text = 'Tambah Kontak'
+            self.memuat_kontak()
 
-    def delete_contact(self, c_id):
+    def hapus_kontak(self, c_id):
         conn = sqlite3.connect('contacts.db')
         c = conn.cursor()
         c.execute('DELETE FROM contacts WHERE id = ?', (c_id,))
         conn.commit()
         conn.close()
-        self.load_contacts()
+        self.memuat_kontak()
 
 if __name__ == '__main__':
-    ContactApp().run()
+    AplikasiKontak().run()
